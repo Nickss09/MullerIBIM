d6ef944c70d572aaa8c98350f96fcd817104ff4b
Nickss09 <nicoliemuller0iap@gmail.com>
2025-03-25 15:47:35 -0300

    Configuração

--- src/App.jsx
+++ src/App.jsx
@@ -18,91 +18,91 @@ function App() {
 </div> */}
    <main className="gallery">
    <div className="card" data-status="Esgotado">
-        <img src="https://i.pinimg.com/736x/d6/56/ac/d656ac49e6a11b1c9b14015a3b190b76.jpg" className="Q"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" className="Q"/>
         <button className="buy-button">Comprar</button>
         <p className="price">$200.00</p>
     </div>
-{/* <div className="card" data-status="">
-        <img src="https://i.pinimg.com/736x/0a/bc/35/0abc35a58abec8f491b49c481def3a66.jpg" className="Q"/>
+<div className="card" data-status="">
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" className="Q"/>
         <button className="buy-button">Comprar</button>
       <p className="price">$259.50</p>
     </div>
     <div className="card" data-status="20% off">
-        <img src="https://i.pinimg.com/236x/4d/72/e2/4d72e2527fab5b9ad789c6b6b406e05d.jpg" className="Q"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" className="Q"/>
         <button className="buy-button">Comprar</button>
       <p className="price">$200.00</p>
     </div>
     <div className="card" data-status="Esgotado">
-        <img src="https://i.pinimg.com/236x/1f/e2/78/1fe2786573e166cc1a4f8c315b17f786.jpg" className="Q"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" className="Q"/>
         <button className="buy-button">Comprar</button>
       <p className="price">$290.00</p>
     </div>
     <div className="card" data-status="">
-        <img src="https://i.pinimg.com/236x/19/7b/41/197b4180394faf2b09aade7096bee20a.jpg" className="Q"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" className="Q"/>
         <button className="buy-button">Comprar</button>
         <p className="price">$900.00</p>
       </div>
       <div className="card" data-status="Esgotado">
-        <img src="https://i.pinimg.com/736x/00/2e/35/002e35d6a26ed459d8efcbd43837c20e.jpg" className="Q"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" className="Q"/>
         <button className="buy-button">Comprar</button>
         <p className="price">$1000.00</p>
       </div>
       
       <div className="card" data-status="">
-        <img src="https://i.pinimg.com/736x/98/44/7a/98447a075a2291a3bd02557893a791ec.jpg" className="Q"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" className="Q"/>
         <button className="buy-button">Comprar</button>
         <p className="price">$1600.00</p>
       </div>
       <div className="card" data-status="20% off">
-        <img src="https://i.pinimg.com/736x/e2/70/28/e27028e368146d7cf1322180653ca34f.jpg" className="Q"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" className="Q"/>
         <button className="buy-button">Comprar</button>
         <p className="price">$3400.000</p>
       </div>
       <div className="card" data-status="Esgotado">
-        <img src="https://i.pinimg.com/736x/e9/49/95/e9499572cd6d4ad5d848e0e551a3ba43.jpg" className="Q"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" className="Q"/>
         <button className="buy-button">Comprar</button>
         <p className="price">$7800.00</p>
       </div>
       <div className="card" data-status="">
-        <img src="https://i.pinimg.com/736x/e5/47/dc/e547dc74236017ffff4850b6aa14d2ba.jpg" className="Q"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" className="Q"/>
         <button className="buy-button">Comprar</button>
         <p className="price">$2900.000</p>
       </div>
       <div className="card" data-status="20% off">
-        <img src="https://i.pinimg.com/736x/a2/f0/7c/a2f07cdce8fb65c922fed942c557436c.jpg" className="Q"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" className="Q"/>
         <button className="buy-button">Comprar</button>
         <p className="price">$900.00</p>
       </div>
       <div className="card" data-status="">
-        <img src="https://i.pinimg.com/736x/23/03/c5/2303c51acded53e960e20e06f02146ce.jpg" className="Q"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" className="Q"/>
         <button className="buy-button">Comprar</button>
         <p className="price">$9000.00</p>
       </div>
- */}
+
    </main>
-   {/* <footer className="footer">
+   <footer className="footer">
     <div className="logo-footer">
-      <img src="https://yt3.googleusercontent.com/k6PuePE0iigmugaaWu3dO89BsWvATAQv3rD3nEY5lu78bgzupAPS0zFaF1K5nuGgXQl_ukmXVA=s160-c-k-c0x00ffffff-no-rj" 
+      <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" 
            alt="Logo Louis Vuitton"/>
     </div>
     <p>Nos siga nas nossas redes sociais</p>
     <div className="social-icons">
       <a href="https://pt-br.facebook.com/LouisVuitton/events/">
-        <img src="https://i.pinimg.com/736x/94/d9/3a/94d93aa1d0dbc775787d26f412431d12.jpg" alt="Facebook"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" alt="Facebook"/>
       </a>
       <a href="https://www.instagram.com/louisvuitton/">
-        <img src="https://i.pinimg.com/736x/3f/cc/39/3fcc393efcfa77d0424970e67f4f97c3.jpg" alt="Instagram"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" alt="Instagram"/>
       </a>
       <a href="https://br.linkedin.com/company/louis-vuitton">
-        <img src="https://i.pinimg.com/736x/19/40/ab/1940abcfbcbdbdb11b1b8fdbf2f01973.jpg" alt="LinkedIn"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" alt="LinkedIn"/>
       </a>
       <a href="https://www.youtube.com/channel/UC5q0PIKGr2lGOsiT14AlEYg">
-        <img src="https://i.pinimg.com/736x/78/91/ee/7891ee94ef06cd85bf3aa3e3bd2deadf.jpg" alt="YouTube"/>
+        <img src="https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png" alt="YouTube"/>
       </a>
     </div>
     <p>Louis Vuitton FashionDay</p>
     <p>101 Champs-Élysées, Paris/FR - 01234-5678</p>
-  </footer> */}
+  </footer>
     </>
   )
 }
