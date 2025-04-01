088dc40ce06bf29dd5fbf241ce559c6ba3a7e598
Nickss09 <nicoliemuller0iap@gmail.com>
2025-04-01 15:53:29 -0300

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -9,84 +9,29 @@ function App() {
       <Header />
       <main className="gallery">
       <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
+        produto='Bolsa - Pochette Métis'
+        descricao='R$ 17.700,00'
+        link='https://br.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-pochette-metis--M44875_PM2_Front%20view.jpg'
       />
       <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
+        produto='Bolsa - Slouchy MM Monogram'
+        descricao='R$ 18.000,00'
+        link='https://br.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-bolsa-slouchy-pm--M11952_PM2_Front%20view.jpg'
       />
       <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
+        produto='Bolsa - Speedy Bandoulière'
+        descricao='R$ 11.900,00'
+        link='https://br.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-bolsa-speedy-bandouliere-25--M46977_PM2_Front%20view.jpg'
       />
       <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
+        produto='Bolsa - Boulogne'
+        descricao='R$ 16.800,00'
+        link='https://br.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-bolsa-boulogne--M45832_PM2_Front%20view.jpg'
       />
       <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
-      />
-      <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
-      />
-      <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
-      />
-      <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
-      />
-      <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
-      />
-      <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
-      />
-      <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
-      />
-      <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
-      />
-      <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
-      />
-      <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
-      />
-      <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
-      />
-      <Card
-        produto='flor'
-        descricao='Tulipa'
-        link='https://gru.ifsp.edu.br/images/phocagallery/galeria2/image04_grd.png'
+        produto='Bolsa Croissant MM'
+        descricao='R$ 20.500,00'
+        link='https://br.louisvuitton.com/images/is/image/lv/1/PP_VP_M/louis-vuitton--M46828_PM2_Front%20view.jpg?wid=750&hei=870'
       />
         </main>
       <Footer />
